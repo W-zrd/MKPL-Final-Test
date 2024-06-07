63a101153732a110919c64497622071e7cf272db
flxnzz <itszyro.47@gmail.com>
2024-06-07 10:07:13 +0700

    Release v2.0

--- src/main/java/pabrikan_1302210127/YamahaMoi_1302210127.java
+++ src/main/java/pabrikan_1302210127/YamahaMoi_1302210127.java
@@ -2,29 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pabrikan_1302213089;
+package pabrikan_1302210127;
 import java.time.LocalDateTime;
-import kendaraan_1302213089.SepedaMotor_1302213089;
+
+import kendaraan_1302210127.SepedaMotor_1302210127;
 
 /**
  *
  * @author hp
  */
-public class HandoBaet_1302213089 extends SepedaMotor_1302213089{
+public class YamahaMoi_1302210127 extends SepedaMotor_1302210127{
     private String warnaMotor;
     private LocalDateTime waktuIsiBensin;
     
-    public HandoBaet_1302213089(String warna, String color){
+    public YamahaMoi_1302210127(String warna, String color){
         super(color);
         warnaMotor = warna;
-        ukuranTangki = 4;
+        ukuranTangki = 5;
         waktuIsiBensin = null;
     }
     
     public void jalan_1302213089(){
         String habis = cekKapanHabis_1302213089();
         if (habis != ""){
-            System.out.println("Sepeda motor sedang berjalan \nMotor Baet akan habis bensin pada " + habis);
+            System.out.println("Sepeda motor sedang berjalan \nMotor Moi akan habis bensin pada " + habis);
         } else {
             System.out.println("Bensin habis, sepeda motor tidak dapat berjalan");
         }
